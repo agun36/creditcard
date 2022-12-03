09850130565321a84fb44aaed7084f49ecc0d858
agun akindele <aguntoroti10@gmail.com>
2022-12-03 11:43:33 +0100

    first commit

--- Server/serverclient.js
+++ Server/serverclient.js
@@ -1,61 +1,46 @@
 const form = document.querySelector('.form')
-console.log(form)
 const btnform = document.querySelector('.btnform')
 const cardname = document.querySelector('.cardname')
-console.log(cardname)
 const cardnumber = document.querySelector('.cardnumber')
-console.log(cardnumber)
 const month = document.querySelector('.month')
 const year = document.querySelector('.year')
 const cvc = document.querySelector('.cvcnum')
 const thankyou = document.querySelector('.thank_you--message')
 const contbtn = document.querySelector('.thankyou-btn')
-console.log(contbtn)
 const displayNumber = document.querySelector('.number')
 const displayName = document.querySelector('#name')
 const displayMonth = document.querySelector('#month')
 const displayYear = document.querySelector('#year')
 const displayCvc = document.querySelector('#cvc')
-
 let regEx =
   /^5[1-5][0-9]{14}$|^2(?:2(?:2[1-9]|[3-9][0-9])|[3-6][0-9][0-9]|7(?:[01][0-9]|20))[0-9]{12}$/
-// email.addEventListener('input', (event) => {
-//   if (email.validity.typeMismatch) {
-//     email.setCustomValidity('I am expecting an e-mail address!')
-//     email.reportValidity()
-//   } else {
-//     email.setCustomValidity('')
-//   }
-// })
 const isValidName = (name) => {
   const re = /^[a-zA-Z ]+$/
   return re.test(String(name).toUpperCase())
 }
+// error validation
 const setError = (element, message) => {
   const inputControl = element.parentElement
-  console.log(inputControl)
   const errorDisplay = inputControl.querySelector('.error')
-
-  console.log(errorDisplay)
   errorDisplay.innerText = message
   errorDisplay.style.color = 'red'
   inputControl.classList.add('error')
   inputControl.classList.remove('success')
 }
+// success validation
 const setSuccess = (element, message) => {
   const inputControl = element.parentElement
   const successDisplay = inputControl.querySelector('.success')
-
   successDisplay.innerText = message
   successDisplay.style.color = 'green'
   inputControl.classList.add('success')
   inputControl.classList.remove('error')
 }
+// input validation
 const validateInputs = () => {
   const nameValue = cardname.value
   const numberValue = cardnumber.value
   const monthValue = month.value
-  console.log(monthValue)
   const yearValue = year.value
   const cvcValue = cvc.value
 
@@ -69,13 +54,13 @@ const validateInputs = () => {
     setSuccess(cardname, 'success')
   }
   // number set error
+  let cardno = /^(?:5[1-5][0-9]{14})$/
 
   if (!numberValue && numberValue === '') {
+    numberValue.substring(0, 16)
     setError(cardnumber, 'cant be empty')
-  } else if (isNaN(numberValue)) {
-    setError(cardnumber, 'card number is not a number')
-  } else if (numberValue.length < 16) {
-    setError(cardnumber, 'card number must be 16')
+  } else if (!cardno.test(numberValue)) {
+    setError(cardnumber, 'card number must start with 55 and must be 16')
   } else {
     setSuccess(cardnumber, 'success')
   }
@@ -91,7 +76,6 @@ const validateInputs = () => {
   }
   // year set error
   let current_year = new Date().getFullYear()
-
   if (!yearValue && yearValue === '') {
     setError(year, 'cant be empty')
   } else if (isNaN(yearValue)) {
@@ -107,16 +91,14 @@ const validateInputs = () => {
   if (!cvcValue && cvcValue === '') {
     setError(cvc, 'cant be empty')
   } else if (isNaN(cvcValue)) {
-    setError(cvc, 'cvc number is not a number')
+    setError(cvc, 'cvc  is not a number')
   } else if (cvcValue.length != 3) {
-    setError(cvc, 'cvc number is too short')
+    setError(cvc, 'cvc length is too short')
   } else {
     setSuccess(cvc, 'success')
   }
-
-  // form.style.display = 'none'
 }
-
+// add eventlistener to confirm button
 btnform.addEventListener('click', (e) => {
   e.preventDefault()
   validateInputs()
@@ -143,6 +125,7 @@ btnform.addEventListener('click', (e) => {
       ? cvc.focus()
       : form == ''
   }
+
   displayNumber.innerHTML = cardnumber.value
   displayNumber.style.display = 'flex'
   displayName.innerHTML = cardname.value
@@ -150,7 +133,7 @@ btnform.addEventListener('click', (e) => {
   displayYear.innerHTML = year.value
   displayCvc.innerHTML = cvc.value
 })
-
+// add eventlistener to success button
 contbtn.addEventListener('click', (e) => {
   e.preventDefault()
   form.style.display = 'flex'
